f2989fcead468e4c3a511747f2f904ba4a1c4036
Masato Hagiwara <hagisan@gmail.com>
2024-12-20 15:13:03 -0800

    Update

---
+++ README.md
@@ -0,0 +1,11 @@
+## SOTALingua: An Expert-Curated Translation Dataset for the State-of-the-Art AI Domain
+
+SOTALingua is an expert-curated translation dataset for AI-specific terms in fields like NLP, computer vision, and robotics.
+
+* A glossary focused on AI-specific terminology.
+* High-quality translations by experts for accuracy.
+* Includes terms from the latest AI research and technologies.
+
+This glossary is the first step toward creating benchmarks to evaluate and improve translations for AI content.
+
+The dataset is released under the CC-BY-NC-SA 4.0 license.
--- sort_glossary.py
+++ sort_glossary.py
@@ -1,15 +1,26 @@
 import yaml
 
+with open('glossary.yaml', 'r') as f:
+    glossary = yaml.safe_load(f)
+
+# check for duplicates
+terms = set()
+for item in glossary:
+    term = item['en'] if isinstance(item['en'], str) else item['en'][0]
+    if term in terms:
+        assert False, f'Duplicate term: {term}'
+    terms.add(term)
+
+
+# sort by term
 def sort_key(x):
     if isinstance(x['en'], str):
         return x['en'].lower()
     return x['en'][0].lower()
 
-glossary_sorted = []
-with open('glossary.yaml', 'r') as f:
-    glossary = yaml.safe_load(f)
-    glossary_sorted = sorted(glossary, key=sort_key)
+glossary_sorted = sorted(glossary, key=sort_key)
 
+# write to file
 yaml_str = yaml.dump(
     glossary_sorted,
     default_flow_style=False,
